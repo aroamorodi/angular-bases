adf08094c5295305b2c35c89e36f61a8640a57d5
aroamorodi <aroa.moro-diaz@capgemini.com>
2024-05-21 10:59:04 +0200

    Comunicación entre padre e hijo con @Output()

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -12,4 +12,12 @@ export class ListComponent {
     name: 'Trunk',
     power: 10
   }]
+
+  @Output()
+  public onDeleteId: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void {
+    //Emitir el ID del personaje
+    this.onDeleteId.emit(index);
+  }
 }
